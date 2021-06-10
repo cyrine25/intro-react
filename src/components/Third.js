5c32f77b06d208eaba721da735016712e90ecd47
cyrine <amricyrine@gmail.com>
2021-06-10 12:10:44 +0100

    first commit

---
+++ src/components/Third.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Third = () => {
+    return (
+        <div className="col-sm-4">
+            <h1 className="Goo">GOMYCODE</h1>
+            <p>groupe f1</p>
+            
+        </div>
+    )
+}
+
+
+export const Fourth = () => {
+    return (
+        <div style={{backgroundColor:"grey",fontSize:"12px"}}>
+            <h3>CopyRights Cyrine</h3>
+        </div>
+    )
+}
+
